691d4ced55c63da125992358f3adf3b442a58059
riproskaie <68151998+riproskaie@users.noreply.github.com>
2024-01-24 13:11:53 +0900

    2024년 01월 24일 수 오후  1:11:53

--- frontend/react-dobongi/src/App.js
+++ frontend/react-dobongi/src/App.js
@@ -1,51 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-function Header(props) {
-  console.log("props", props.title);
-  return (
-    <header>
-      <h1><a href="/">{props.title}</a></h1>
-    </header>
-  )
-}
-
-function Nav(props) {
-  const lis = []
-  for(let i=0; i<props.topics.length; i++) {
-    let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>);    
-  }
-  return (
-    <nav>
-      <ol>
-        {lis}     
-      </ol>
-    </nav>
-  )
-}
-
-function Article(props) {
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      {props.body}
-    </article>
-  )
-}
+// component 폴더는 src 밑에 오는지 확인할 것
+import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
 
 function App() {
-  const topics = [
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'},
-  ]
-  return (
-    <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
-      <Article title="Welcome" body="Hello, Web"></Article>
-    </div>
+  return (  
+  <div className="App">
+    <Header></Header>
+    <Footer></Footer>
+  </div>
   );
 }
 
